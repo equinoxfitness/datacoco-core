aa87c6739c34a6567e4b9de1c7816927a88db8bd
lciamp <louis.ciampanelli@equinox.com>
2019-11-25 16:20:31 -0500

    added read the docs support

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from distutils.core import setup
 
 setup(
-  name = 'datacoco_core',
-  packages = ['datacoco_core'],
-  version = '0.1.0',
-  license='MIT',
-  description = 'Core features of common code utility',
-  author = 'Equinox Fitness',
-  url = 'https://github.com/equinoxfitness/datacoco-core',
-  download_url = 'https://github.com/equinoxfitness/datacoco-core/archive/v-0.1.0.tar.gz',
-  keywords = ['helper', 'config', 'logging', 'common'],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.7',
-  ],
+    name="datacoco_core",
+    packages=["datacoco_core"],
+    version="0.1.0",
+    license="MIT",
+    description="Core features of common code utility",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Equinox Fitness",
+    url="https://github.com/equinoxfitness/datacoco-core",
+    download_url="https://github.com/equinoxfitness/datacoco-core/archive/v-0.1.0.tar.gz",
+    keywords=["helper", "config", "logging", "common"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
